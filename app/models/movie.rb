e7ca77404bfcc6e990aab042c4821d727cfacd32
Alexandre Bouvier <alexandre.b2506@gmail.com>
2020-05-28 09:51:21 +0200

    Minor changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ApplicationRecord
+  searchkick
   belongs_to :director
+  include PgSearch::Model
+  # pg_search_scope :search_by_title_and_syllabus,
+  #   against: [ :title, :syllabus ],
+  #   associated_against: {
+  #     director: [ :first_name, :last_name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
+  multisearchable against: [:title, :syllabus]
 end
